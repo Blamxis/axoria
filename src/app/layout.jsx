372956daa67c91766221f44ee2a8c88f708e30f0
Blamxis <gavinetm26@gmail.com>
2025-07-06 16:33:41 +0200

    Add NavBar and Footer to layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,11 +1,17 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
+import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="fr" className="h-full">
+    <html lang="en" className="h-full">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <body>{children}</body>
+      <body className="flex min-h-full flex-col">
+        <Navbar />
+        <main className="grow">{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
