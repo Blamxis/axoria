14c9eee345b721b551ab020956052aa651c7460b
Blamxis <gavinetm26@gmail.com>
2025-07-07 15:27:26 +0200

    Add form create article

---
+++ src/app/dashboard/create/page.jsx
@@ -0,0 +1,48 @@
+"use client";
+function page() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+  }
+
+  return (
+    <main className="u-main-container bg-white p-7 mt-32 mb-44">
+      <h1 className="text-4xl mb-4">Write an article 📝</h1>
+      <form onSubmit={handleSubmit} className="pb-6">
+        <label htmlFor="title" className="f-label">
+          Title
+        </label>
+        <input
+          type="text"
+          name="title"
+          className="shadow border rounded w-full p-3 mb-7 text-gray-700 focus:outline-slate-400"
+          id="title"
+          placeholder="Title"
+          required
+        />
+        <label htmlFor="markdownArticle" className="f-label">
+          Write your article using markdown - do not repeat the already given
+          title
+        </label>
+        <a
+          target="_blank"
+          href="https://www.markdownguide.org/cheat-sheet/"
+          className="block mb-4 text-blue-600"
+        >
+          How to use the markdown syntax ?
+        </a>
+        <textarea
+          name="markdownArticle"
+          id="markdownArticle"
+          required
+          className="min-h-44 text-xl shadow appearance-none border rounded w-full p-8 text-gray-700 mb-4 focus:outline-slate-400"
+        ></textarea>
+        <button className="min-w-44 bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-3 px-4 rounded border-none mb-4">
+          Submit
+        </button>
+      </form>
+    </main>
+  );
+}
+
+export default page;
